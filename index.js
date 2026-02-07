0c124ede13b216d1e550a77e0142a70ecf15a709
abdulgondal373-max <abdul.gondal373@gmail.com>
2026-02-07 22:13:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const express = require('express'); // Ajout du serveur
 
+const app = express();
+const port = process.env.PORT || 10000;
+
+// Petit serveur pour Render et UptimeRobot
+app.get('/', (req, res) => {
+    res.send('Le bot est en ligne !');
+});
+
+app.listen(port, () => {
+    console.log(`Serveur de maintien en éveil sur le port ${port}`);
+});
+
+// Ton code de Bot Discord
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
     ],
-    partials: [Partials.Channel] // Nécessaire pour capter les messages privés (DMs)
+    partials: [Partials.Channel]
 });
 
 client.once('ready', () => {
-    console.log(`Bot en ligne !`);
+    console.log(`Bot prêt !`);
 });
 
 client.on('messageCreate', async (message) => {
-    // Ignore les messages des bots
     if (message.author.bot) return;
-
-    // Si le message n'est pas dans un serveur (donc en DM)
     if (!message.guild) {
         try {
             await message.author.send("Salut, tiens le lien pour les matchs ➡️ https://t.me/+utGMq_cWFRplMTI0");
-            console.log(`Réponse envoyée à ${message.author.tag}`);
         } catch (error) {
-            console.error("Erreur lors de l'envoi du DM :", error);
+            console.error("Erreur DM:", error);
         }
     }
 });
 
 client.login(process.env.TOKEN);
+
