97afe40521799bed2c5b3b5a697371fd2215f3b4
abdulgondal373-max <abdul.gondal373@gmail.com>
2026-02-15 15:02:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 10000;
-app.get('/', (req, res) => res.send('Bot HIBOUX en mode auto !'));
+app.get('/', (req, res) => res.send('Bot AKH en mode auto !'));
 app.listen(port);
 
 const MON_ID = "744871541715632138"; 
@@ -21,14 +21,14 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-    client.user.setActivity('DM pour lien', { type: ActivityType.Watching });
-    console.log("Le bot est prêt et n'attend plus que des DMs !");
+    client.user.setActivity('DM pour avoir le lien', { type: ActivityType.Watching });
+    console.log("Le bot AKH est prêt !");
 });
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
-    // TA COMMANDE PRIVÉE (uniquement pour toi en DM avec le bot)
+    // COMMANDE PRIVÉE : !dire [ID] [Message]
     if (message.author.id === MON_ID && message.content.startsWith('!dire')) {
         const args = message.content.split(' ');
         const cibleId = args[1];
@@ -41,11 +41,9 @@ client.on('messageCreate', async (message) => {
         return;
     }
 
-    // RÉPONSE AUTOMATIQUE EN DM (pour tout le monde)
+    // RÉPONSE AUTO EN DM
     if (!message.guild) {
-        // Affiche l'ID du mec dans tes logs Render pour que tu puisses lui répondre
-        console.log(`Message de ${message.author.tag} | ID: ${message.author.id}`);
-        
+        console.log(`Message pour AKH de ${message.author.tag} | ID: ${message.author.id}`);
         await message.author.send(`Salut, tiens le lien pour les matchs ➡️ ${LIEN_TELEGRAM}`);
     }
 });
